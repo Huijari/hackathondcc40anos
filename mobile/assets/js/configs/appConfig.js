a1b897dcd0f120a93cd0dafc406cfc0732a0a283
huijari <huijari@users.noreply.github.com>
2016-08-27 17:12:06 -0300

    Remove whitespace

--- mobile/assets/js/configs/appConfig.js
+++ mobile/assets/js/configs/appConfig.js
@@ -23,7 +23,7 @@ app.config(function($routeProvider, $locationProvider){
 		firebase.initializeApp(config);
 
 		$locationProvider.html5Mode({
-			enabled: true,
+			enabled: false,
 			requireBase: false
 		});
 
@@ -53,15 +53,15 @@ app.config(function($routeProvider, $locationProvider){
 });
 
 app.run(function($location){
-		
+
 
 	firebase.auth().getRedirectResult().then(function(result){
 		if (result.user) {
 			$location.path(window.location.pathname +'classesList');
 		}
-	}); 
+	});
 
-		
+
 });
 
 })();

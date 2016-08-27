7f2391689a37821dc4df2eb1ccea3b20347a0cfc
vitorltb <vitorltb@users.noreply.github.com>
2016-08-27 10:00:37 -0300

    initial configs/ project structure

---
+++ mobile/assets/js/configs/appConfig.js
@@ -0,0 +1,24 @@
+(function () {
+
+var app = angular.module("ClassPictures", ["ngRoute", "ngMaterial"]);
+
+app.config(function($routeProvider, $locationProvider){
+		var initialPath = window.location.pathname;
+
+		$locationProvider.html5Mode({
+			enabled: true,
+			requireBase: false
+		});
+
+		$routeProvider
+			// .when(initialPath, {
+			// 	templateUrl : 'assets/templates/login.html'
+			// })
+			.otherwise({
+				redirectTo: initialPath
+			});
+});
+
+})();
+
+

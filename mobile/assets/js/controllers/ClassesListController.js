737e9cd0b4a3097a2f1191f7ed8f83b607e2019c
meninosimas <meninosimas@users.noreply.github.com>
2016-08-28 05:35:14 -0300

    upload direct image/hour

--- mobile/assets/js/controllers/ClassesListController.js
+++ mobile/assets/js/controllers/ClassesListController.js
@@ -2,9 +2,9 @@
 
 var app = angular.module('ClassPictures');
 
-app.controller('ClassesListController', ['$scope', '$location', ClassesListController]);
+app.controller('ClassesListController', ['$scope', '$location', 'Image', 'Class', 'UserService', ClassesListController]);
 
-function ClassesListController($scope, $location) {
+function ClassesListController($scope, $location, Image, Class, UserService) {
 	
 	var setMessage = function(){
 		var message;
@@ -18,6 +18,44 @@ function ClassesListController($scope, $location) {
 
 	$scope.addNewClassClick = function(){
 		$location.path("/selectClasses");
+	};
+
+	$scope.checkUploadByDate = function() {
+		UserService.getUserClasses(firebase.auth().currentUser.uid).on('value', function(snapshot) {
+			var classes = snapshot.val();
+			var dias = {Dom: 0, Seg: 1, Ter: 2, Qua: 3, Qui: 4, Sex: 5, Sab: 6};
+			if (classes) {
+				Object.keys(classes).forEach(function(key) {
+					Class.getById(classes[key].id).on('value', function(snapshot) {
+						var classe = snapshot.val();
+						if(classe) {
+							classe.dia_semana = (classe.dia_semana.indexOf('-') == -1)? [classe.dia_semana] : classe.dia_semana.split('-');
+							classe.dia_semana.forEach(function(dia) {
+								if(dias[dia] == moment().day() && moment({hour: hora_inicial.split(':')[0], minute: hora_inicial.split(':')[1]}) <= moment() && moment() <= moment({hour: hora_final.split(':')[0], minute: hora_final.split(':')[1]})) {
+										$scope.currentClass = classe.id;
+										return true;
+								}
+							});
+						}
+					});
+				});
+			}
+			return false;
+		});
+	};
+
+	$scope.uploadPhoto = function(e) {
+	    var imageId = (new Date()).getTime()+'';
+	    Image.getImage($scope.currentClass, imageId).put(e.files[0]);
+	    Class.getById($scope.currentClass).child('images').push({
+	        id: imageId,
+	        owner: {
+	            id: firebase.auth().currentUser.uid,
+	            name: firebase.auth().currentUser.displayName
+	        },
+	        description: '',
+	        isPublic: true
+	    });
 	};
 
 	this.countMemberClass = function countMemberClass(group) {

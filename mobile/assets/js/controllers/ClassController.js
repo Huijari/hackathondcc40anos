4e7d67a812c9e21a044cec55ac545495e519144f
huijari <huijari@users.noreply.github.com>
2016-08-28 04:53:43 -0300

    Fix .2

--- mobile/assets/js/controllers/ClassController.js
+++ mobile/assets/js/controllers/ClassController.js
@@ -73,7 +73,7 @@ function ClassController($scope, $routeParams, $location, Class, Image) {
     context.drawImage(image, 0, 0, canvas.width, canvas.height);
     canvas.className = 'ng-hide';
     document.body.appendChild(canvas);
-    var quality = .2;
+    var quality = 0.2;
     var dataURI = canvas.toDataURL('image/jpeg', quality);
     var binary = atob(dataURI.split(',')[1]);
     var array = [];
